ccc7efba0f6ac9eb3e307ee6cbe1ab2343a2316c
Y.Wang <shengguanglong01@sina.com>
2019-02-14 17:11:25 +0800

    fix bug and create "GuideWindow.cpp" from .h

--- main.cpp
+++ main.cpp
@@ -1,17 +1,6 @@
-#include "Headers/GuideWindow.h"
+#include "GuideWindow.cpp"
 
-void Ui_GuideWindow::setEvents()
-{
-    QObject::connect(button_left_1, &QAbstractButton::clicked, [=]() mutable { stackedWidget->setCurrentIndex(0); });
-    QObject::connect(button_left_2, &QAbstractButton::clicked, [=]() mutable { stackedWidget->setCurrentIndex(1); });
-    QObject::connect(button_left_3, &QAbstractButton::clicked, [=]() mutable { stackedWidget->setCurrentIndex(2); });
-    QObject::connect(button_left_4, &QAbstractButton::clicked, [=]() mutable { stackedWidget->setCurrentIndex(3); });
-    QObject::connect(info_button_1, &QAbstractButton::clicked, [=]() mutable {});
-    QObject::connect(info_button_2, &QAbstractButton::clicked, [=]() mutable {});
-    QObject::connect(search_button_1, &QAbstractButton::clicked, [=]() mutable {});
-    QObject::connect(road_button_1, &QAbstractButton::clicked,[=]() mutable {});
-    QObject::connect(multi_button_1, &QAbstractButton::clicked,[=]() mutable {});
-}
+
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
